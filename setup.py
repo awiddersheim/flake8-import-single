44c293309299ca0ae4aaf726f340e7d78a58cd34
Andrew Widdersheim <amwiddersheim@gmail.com>
2018-07-26 11:26:17 -0400

    Foo

--- setup.py
+++ setup.py
@@ -51,7 +51,6 @@ setup(
             'flake8-quotes',
             'pytest',
             'pytest-cov',
-            'pytest-sugar',
         ],
     },
     entry_points={
@@ -68,9 +67,15 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Software Development :: Quality Assurance',
         'Operating System :: OS Independent'
     ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
 )

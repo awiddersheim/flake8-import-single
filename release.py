7ef9ecaa27981d589ecd18e75ad16aaeed3019d8
Andrew Widdersheim <amwiddersheim@gmail.com>
2018-08-03 14:33:07 -0400

    Fix release

--- release.py
+++ release.py
@@ -6,30 +6,29 @@ import subprocess
 from github import Github
 
 
+def run_command(command):
+    return subprocess.check_output(
+        command,
+        shell=True
+    ).strip().decode('utf-8')
+
+
 github = Github(os.environ['GITHUB_TOKEN'])
 
 repo = github.get_user().get_repo(
-    os.path.basename(
-        os.path.dirname(
-            os.path.realpath(__file__),
-        ),
-    ),
+    run_command('python setup.py --name'),
 )
 
-tag = subprocess.check_output(
-    'python setup.py --version',
-    shell=True,
-).strip().decode('utf-8')
+tag = run_command('python setup.py --version')
 
 repo.create_git_release(
     tag=tag,
     name=tag,
-    message=subprocess.check_output(
+    message=run_command(
         'git show --no-patch --pretty=format:"%s%n%b" {}'.format(
                 os.environ['CIRCLE_SHA1'],
             ),
-            shell=True,
-        ).strip().decode('utf-8'),
+        ),
     draft=True,
     prerelease=os.environ.get('CIRCLE_TAG') is None,
     target_commitish=os.environ.get('CIRCLE_TAG', os.environ['CIRCLE_SHA1'])

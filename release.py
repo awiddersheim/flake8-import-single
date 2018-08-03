e1f567829f3a28acc24998ada94113208fab0077
Andrew Widdersheim <amwiddersheim@gmail.com>
2018-08-03 15:29:10 -0400

    Fix deploy

--- release.py
+++ release.py
@@ -15,25 +15,29 @@ def run_command(command):
     ).strip().decode('utf-8')
 
 
-github = Github(os.environ['GITHUB_TOKEN'])
+def main()
+    github = Github(os.environ['GITHUB_TOKEN'])
 
-repo = github.get_user().get_repo(
-    run_command('python setup.py --name'),
-)
+    repo = github.get_user().get_repo(
+        run_command('python setup.py --name'),
+    )
 
-tag = 'v{}'.format(run_command('python setup.py --version'))
+    tag = 'v{}'.format(run_command('python setup.py --version'))
 
-release = repo.create_git_release(
-    tag=tag,
-    name=tag,
-    message=run_command(
-        'git show --no-patch --pretty=format:"%s%n%b" {}'.format(
-                os.environ['CIRCLE_SHA1'],
+    release = repo.create_git_release(
+        tag=tag,
+        name=tag,
+        message=run_command(
+            'git show --no-patch --pretty=format:"%s%n%b" {}'.format(
+                    os.environ['CIRCLE_SHA1'],
+                ),
             ),
-        ),
-    prerelease=os.environ.get('CIRCLE_TAG') is None,
-    target_commitish=os.environ.get('CIRCLE_TAG', os.environ['CIRCLE_SHA1'])
-)
+        target_commitish=os.environ['CIRCLE_SHA1'],
+    )
 
-for item in glob.glob(sys.argv[1]):
-    release.upload_asset(item)
+    for item in glob.glob(sys.argv[1]):
+        release.upload_asset(item)
+
+if __name__ == '__main__':
+    if os.environ.get('CIRCLE_TAG'):
+        main()
